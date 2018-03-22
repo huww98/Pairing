3f8cc6648958bbc9c588918664aba84527ef29ce
胡玮文 <huww98@163.com>
2018-03-22 11:51:05 +0800

    添加项目文件。

---
+++ Pairing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pairing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
